30b9786e1fba3cc9c56f7ea73d6eea08161d23e4
Adam Espinoza <adespinoza97@gmail.com>
2015-06-01 09:46:39 -0700

    Updated Code Generator
    Fractal tree finally works as it is supposed to!

--- fractalTree.java
+++ fractalTree.java
@@ -1,5 +1,9 @@
 import javax.swing.*; 
+import java.awt.*;
+import java.awt.Color; 
 import objectdraw.*;
+import java.awt.Graphics; 
+import java.awt.Graphics2D; 
 
 /**
  * Methods for the interactive Fractal Tree 
@@ -7,52 +11,79 @@ import objectdraw.*;
  * @Adam Espinoza & Samuel Garcia
  * @2015-26-05
  */
-public class fractalTree extends JFrame
+public class FractalTree extends JComponent
 {
-    private JPanel panel;
-    private int iterations;
-    private double theta; 
-    private double length;
-    private int width; 
-    private int height; 
+    private JPanel panel;                                   //NOT SURE IF I NEED THIS?!?!?!?!?!
 
+    private double theta;                                   //Angle which branches divert
+    private int length;                                     //Length of the starting line
+    private final int WINDOW_WIDTH = 1280;                  //Width of the Canvas 
+    private final int WINDOW_HEIGHT = 720;                 //Height of the Canvas 
+
+    private double x1;                                       //Starting x-coordinate  
+    private double y1;                                       //Starting y-coordinate 
+    
+    public static void main(String[] args)
+    {
+        FractalTree fractalTree = new FractalTree(); 
+        fractalTree.setVisible(true); 
+        fractalTree.setBackground(Color.black); 
+    }
+    
     /**
      * Constructor for objects of class fractalTree
-     * @param iterations the maximum number of iterations the fractal will proceed with.
-     * @param length the length at which the factals will begin to diverge.
-     * @param theta the angle at which the fractals branch off the origin fractal.
-     * @param width the width of the canvas for the fractal tree. 
-     * @param height the height of the canvas for the fractal tree. 
+     * Mostly used for creating the window for the thing to be printer. 
      */
-    public fractalTree(int iterations, int length, double theta, int width, int height)
+    public FractalTree()
     {
-        this.iterations = iterations; 
-        this.length = length; 
-        this.theta = theta; 
-        this.width = width; 
-        this.height = height; 
+        JFrame window = new JFrame(); 
+        window.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+        window.setTitle("Fractal Tree Generator"); 
+        window.setLocationRelativeTo(null);
+        window.setResizable(true);
+        window.add(this);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+    }
+       
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        g.setColor(Color.green); 
+        drawFractalLine(g, WINDOW_WIDTH / 2 ,WINDOW_HEIGHT - 75, 12, -90);
     }
     
     /**
      * Method creates branches for the tree
+     * @param x1 the x-coordinate you are starting at.
+     * @param y1 the y-coordinate you are starting at.
+     * @param length the length to which the fractal tree will grow. (Maximum length)
+     * @param theta the angle at which the factals will begin to diverge.
      */
-    public void branches() 
+    public void drawFractalLine(Graphics g, int x1, int y1, int length, double theta) 
     {
-        //float theta = Math.random(Math.PI/3); //creates random angles for the tree to go. 
-        JDrawingCanvas canvas = new JDrawingCanvas(width, height); 
-        Line l = new Line(0,0,0,-100, canvas);
-        l.setStart(0, -length);
-        length *= 0.66;
-        if (length > 2) 
+        if (length == 0) {
+        }
+        else
         {
-            pushMatrix();
-            rotate(theta);
-            branch(length);
-            popMatrix();
-            pushMatrix();
-            rotate(-theta);
-            branch(length);
-            popMatrix();
+            int x2 = x1 + (int) (length * 10 * Math.cos(Math.toRadians(theta)));
+            int y2 = y1 + (int) (length * 10 * Math.sin(Math.toRadians(theta)));
+
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2d.setStroke(new BasicStroke(.75f * length));
+            g2d.drawLine(x1, y1, x2, y2);
+
+            drawFractalLine(g, x2, y2, length - 1, theta - 40);
+            drawFractalLine(g, x2, y2, length - 1, theta + 40);
+          
+            //drawFractalLine(g, x2, y2, length - 1, theta - 60);
+            //drawFractalLine(g, x2, y2, length - 1, theta + 60);
+            
         }   
     }
+
 }
